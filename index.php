cf517c1823bc10fa2fcf7f2d93b346d4852d12ee
Wiam AR <aarikawieam@gmail.com>
2025-07-15 20:05:58 +0100

    first section 1 & navigation

---
+++ index.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./styles/stylle.css">
+    <title>Love Fete</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <nav class="navbar navbar-expand-lg ">
+                        <div class="container-fluid">
+                            <a class="navbar-brand" href="#"><img src="./pics/logo.png" alt=""></a>
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse" id="navbarNav">
+                                <ul class="navbar-nav">
+                                    <li class="nav-item">
+                                        <a class="nav-link active" aria-current="page" href="#home">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#about">about</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#services">services</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#impact">impact</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#testimonials">testimonials</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#contact">contact</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+
+    </header>
+    <!-- Section 1 -->
+    <section id="section1" class="mt-2" style="height:100vh; ">
+        <div class="container  ">
+            <img src="" alt="">
+            <video autoplay muted loop class="hero-section" style="width: 100%; height: 100%; object-fit: cover; position: absolute; top: 0; left: 0; z-index: -1; border-radius: 20px;">
+                <source src="./pics/204803-925552205_tiny.mp4" type="video/mp4">
+            </video>
+
+            <div class="row justify-content-center ">
+                <div class="col-md-10 col-sm-12 centrehero mt-2 mx-auto">
+
+                    <h1 class="hero-title" style="font-size: 3rem; margin-top: 15%;">Rencontre des voyageurs.</h1>
+                    <p class="hero-subtitle">Choisir où te loger et on te montrera avec qui !</p>
+                    <div class="row p-5">
+                        <div class="col"></div>
+                    </div>
+
+
+                </div>
+
+            </div>
+        </div>
+        </div>
+        </div>
+
+        </div>
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</body>
+
+</html>
